210e145d2b66cf802d9e065f897d39b8a28ec612
DoniyorArtykbaev <donerartykbaev@gmail.com>
2024-06-04 08:19:25 +0500

    JS 1-dars
    Zeromax IT-Center 35-guruh

---
+++ js/main.js
@@ -0,0 +1,57 @@
+/*console.log("Men faqat konsol oynasida ko'rinaman!"); */
+
+/* alert("Saytimizga xush kelibsiz!"); */
+
+
+/*document.write(2*2+2-2/2); */
+
+//let x = 5;
+//let y = 7;
+//let z = 10;
+//let salom = "Assalomu alaykum!"
+//let rang = "f1f1f1"
+//let x = 15, y = 7, z  = 10;
+//document.write(x+y+z);
+
+//JSda ma'lumotlar turlari
+//let math = "JavaScript"
+//let son = 45; 
+
+//document.write(typeof math);
+
+//JSda matematika 
+//let x = 5;
+//x-=5;
+//x*=5;
+//  let qoldiq = x%5;
+//++x;
+//x++;
+//x--;
+//--x;
+
+
+document.write(qoldiq)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
